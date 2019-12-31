2bcca0f9ec5d9081cc512dc6a9ac0a1742d017d5
Ross Borchers <ross@renderheads.com>
2019-12-31 14:11:12 +0200

    simplifying and improving boostrapper, state and manager scaffolding

---
+++ Managers/Manager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Boostrapper
+{
+    public abstract class Manager
+    {
+        protected static List<Manager> _globalManagers;
+        protected static Dictionary<string, List<Manager>> _groupedManagers;
+
+        protected Manager()
+        {
+            _globalManagers.Add(this);
+        }
+
+        //Override in manager
+        protected Manager(string managerGroup)
+        {
+            if(_groupedManagers.TryGetValue(managerGroup, out List<Manager> group))
+            {
+                group.Add(this);
+            }
+            else
+            {
+                _groupedManagers.Add(managerGroup, new List<Manager>() { this });
+            }
+        }
+
+        public static T Get<T>() where T : Manager
+        {
+            foreach (Manager m in _globalManagers)
+            {
+                if (typeof(T).IsAssignableFrom(m.GetType()))
+                {
+                    return (T)m;
+                }
+            }
+            return null;
+        }
+
+        public static T Get<T>(string group) where T : Manager
+        {
+            Debug.Assert(string.IsNullOrEmpty(group), "[Manager] group is null or empty when calling Get()");
+
+            if(_groupedManagers.TryGetValue(group, out var value))
+            {
+                foreach(Manager m in value)
+                {
+                    if(typeof(T).IsAssignableFrom(m.GetType()))
+                    {
+                        return (T)m;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public abstract void UpdateManager();
+        public abstract void ShutdownManager();
+    }
+}
---
+++ Managers/StateManager.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Boostrapper
+{
+    //Simplifies state management by providing an easy to use calling paradim with Entered, Updated and Exited events.
+    //Pass an enum and call "For(T state)" to begin building callbacks
+    public class StateManager<T> : Manager where T : struct, IComparable
+    {
+        // protected members 
+        protected class StateHandler
+        {
+            public event Action OnEntered = delegate { };
+            public event Action OnExited = delegate { };
+            public event Action OnUpdated = delegate { };
+
+            public void ExecuteEntered()
+            {
+                OnEntered();
+            }
+            public void ExecuteExited()
+            {
+                OnExited();
+            }
+            public void ExecuteUpdated()
+            {
+                OnUpdated();
+            }
+        }
+
+        //Dict would be slower than list
+        protected List<KeyValuePair<T, StateHandler>> _handlers = new List<KeyValuePair<T, StateHandler>>();
+        protected List<KeyValuePair<T, StateData>> _data = new List<KeyValuePair<T, StateData>>();
+        protected U GetOrCreateValueForKVPList<U>(List<KeyValuePair<T, U>> list, T key, Func<U> onNew) where U : class
+        {
+            U value = null;
+            foreach (KeyValuePair<T, U> kvp in list)
+            {
+                if (kvp.Key.CompareTo(key) == 0)
+                {
+                    value = kvp.Value;
+                }
+            }
+            if (value == null)
+            {
+                U newValue = onNew();
+                list.Add(new KeyValuePair<T, U>(key, newValue));
+                return newValue;
+            }
+            return value;
+        }
+
+        protected StateHandler GetHandler(T state)
+        {
+            return GetOrCreateValueForKVPList(_handlers, state, ()=>new StateHandler());
+        }
+
+        protected bool _stateInitialized;
+
+        protected T _state;
+
+        // Public members
+
+        public class StateData
+        {
+            private StateManager<T> _manager;
+
+            public StateData(StateManager<T> manager)
+            {
+                _manager = manager;
+            }
+
+            public T _state;
+
+            public StateData Entered(Action callback)
+            {
+                StateHandler handler = _manager.GetHandler(_state);
+                handler.OnEntered += callback;
+                return this;
+            }
+
+            public StateData Updated(Action callback)
+            {
+                StateHandler handler = _manager.GetHandler(_state);
+                handler.OnUpdated += callback;
+                return this;
+            }
+
+            public StateData Exited(Action callback)
+            {
+                StateHandler handler = _manager.GetHandler(_state);
+                handler.OnExited += callback;
+                return this;
+            }
+        }
+
+        public StateManager() : base()
+        {
+            _stateInitialized = false;
+        }
+
+        public StateManager(string managerGroup) : base(managerGroup)
+        {
+            _stateInitialized = false;
+        }
+
+        public StateData For(T state)
+        {
+            return GetOrCreateValueForKVPList(_data, state, ()=>new StateData(this));
+        }
+
+        public T State
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                if (value.CompareTo(_state) != 0 || !_stateInitialized)
+                {
+                    if (_stateInitialized)
+                    {
+                        GetHandler(_state).ExecuteExited();
+                    }
+                    _state = value;
+                    GetHandler(_state).ExecuteEntered();
+                    _stateInitialized = true;
+                }
+            }
+        }
+
+        public override void UpdateManager()
+        {
+            GetHandler(_state).ExecuteUpdated();
+        }
+
+        public override void ShutdownManager(){}
+    }
+}
