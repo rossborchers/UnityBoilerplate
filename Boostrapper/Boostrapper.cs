a9427bc6bf75a906c7522041f90c5092c3b843b2
RossRH <ross@renderheads.com>
2020-04-07 23:58:23 +0200

    boostrapper changes

--- Boostrapper/Boostrapper.cs
+++ Boostrapper/Boostrapper.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 
 public abstract class BoostrapperBase : MonoBehaviour
 {
-    protected LevelData LevelData;
+    [SerializeField]
+    protected LevelData _levelData;
 
     private string _activeLevel = null;
     private Stack<string> _activePostloadScenes = new Stack<string>();
@@ -14,7 +15,7 @@ public abstract class BoostrapperBase : MonoBehaviour
     private LevelData.Level GetLevelData(string levelName)
     {
         LevelData.Level level = null;
-        foreach (LevelData.Level l in LevelData.Levels)
+        foreach (LevelData.Level l in _levelData.Levels)
         {
             if (l.LevelName.ToLower().Trim() == levelName.ToLower().Trim())
             {
@@ -44,7 +45,7 @@ public abstract class BoostrapperBase : MonoBehaviour
         }
 
         Busy = true;
-        Action finishedIntercept = () => 
+        Action finishedIntercept = () =>
         {
             Busy = false;
             onFinished?.Invoke();
@@ -54,6 +55,7 @@ public abstract class BoostrapperBase : MonoBehaviour
         {
             Debug.LogWarning("[Bootstrapper] Loading level when there is already one loaded. Can not load multiple levels at once. Unloading before loading." +
                 "If you want multiple levels consider making scenes part of one level stacks. If you want to unload the old level please make it explicit by calling LoadLevel in the onFinished callback of UnloadLevel");
+            Busy = false;
             UnloadLevel(()=>
             {
                 LoadLevel(levelName, finishedIntercept);
@@ -78,9 +80,9 @@ public abstract class BoostrapperBase : MonoBehaviour
         string toLoad = scenes[0];
         scenes.RemoveAt(0);
 
-        SceneManager.LoadSceneAsync(toLoad);
+        SceneManager.LoadSceneAsync(toLoad, LoadSceneMode.Additive);
         SceneManager.sceneLoaded += OnPostLoadLoaded;
-  
+
         void OnPostLoadLoaded(Scene s, LoadSceneMode mode)
         {
             _activePostloadScenes.Push(s.name);
@@ -91,14 +93,27 @@ public abstract class BoostrapperBase : MonoBehaviour
     }
 
     protected void UnloadLevel(Action onFinished = null)
-    {       
+    {
+        if(Busy)
+        {
+            throw new Exception("LoadLevel called while level is busy being loaded. This is not allowed. You can poll BusyLoading if necessary.");
+        }
+
+        Busy = true;
+        Action finishedIntercept = () =>
+        {
+            Busy = false;
+            _activeLevel = null;
+            onFinished?.Invoke();
+        };
+
         if (_activeLevel == null)
         {
             Debug.LogWarning("[Bootstrapper] Unloading level but there are none loaded. Skipping.");
-            onFinished?.Invoke();
+            finishedIntercept();
         }
 
-        UnloadStack(onFinished);
+        UnloadStack(finishedIntercept);
     }
 
     private void UnloadStack(Action onFinished)
@@ -109,9 +124,9 @@ public abstract class BoostrapperBase : MonoBehaviour
             return;
         }
 
-        string toLoad = _activePostloadScenes.Pop();
+        string toUnload = _activePostloadScenes.Pop();
 
-        SceneManager.UnloadSceneAsync(toLoad);
+        SceneManager.UnloadSceneAsync(toUnload);
         SceneManager.sceneUnloaded += OnPostLoadUnloaded;
 
         void OnPostLoadUnloaded(Scene s)
