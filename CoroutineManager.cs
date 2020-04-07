bd8d2cf11f1dcd76918c109c34079a19c08383b5
RossRH <ross@renderheads.com>
2020-04-07 23:56:10 +0200

    Added coroutine manager

---
+++ CoroutineManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoroutineManager : MonoBehaviour
+{
+    private static CoroutineManager _instance = null;
+
+    private Coroutine StartInstanceCoroutine(IEnumerator coroutine)
+    {
+        return base.StartCoroutine(coroutine);
+    }
+
+    private void StopInstanceCoroutine(Coroutine coroutine)
+    {
+        base.StopCoroutine(coroutine);
+    }
+
+    private static void LazyInitInstance()
+    {
+        if (_instance == null)
+        {
+            GameObject mgrObject = new GameObject("CoroutineManager");
+            _instance = mgrObject.AddComponent<CoroutineManager>();
+            DontDestroyOnLoad(mgrObject);
+        }
+    }
+
+    public new static Coroutine StartCoroutine(IEnumerator coroutine)
+    {
+        LazyInitInstance();
+        return _instance.StartInstanceCoroutine(coroutine);
+    }
+
+
+    public new static void StopCoroutine(Coroutine coroutine)
+    {
+        LazyInitInstance();
+        _instance.StopInstanceCoroutine(coroutine);
+    }
+}
